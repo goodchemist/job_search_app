ee958e53a38021d16e47b61e23c9462664a908b9
good_chemist <iolgasergeevna@yandex.ru>
2024-02-13 18:00:40 +0800

    add __repr__ and __str__

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,7 +1,6 @@
 class Vacancy:
-    """
-    Класс для работы с вакансиями.
-    """
+    """Класс для работы с вакансиями."""
+
     def __init__(self, name: str, salary_from: str, salary_to: str, salary_currency: str, city: str, description: str,
                  url_vacancy: str) -> None:
         """
@@ -21,3 +20,20 @@ class Vacancy:
         self.city = city
         self.description = description
         self.url_vacancy = url_vacancy
+
+    def __str__(self) -> str:
+        """
+        Метод для отображения экземпляра класса Vacancy.
+        :return: f-строка с данными экземпляра Vacancy.
+        """
+        return (f'{self.name}, {self.salary_from}, {self.salary_to}, {self.salary_currency}, {self.city}, '
+                f'{self.description}, {self.url_vacancy}')
+
+    def __repr__(self) -> str:
+        """
+        Возвращает строковое представление экземпляра класса Vacancy.
+        :return: f-строка с данными экземпляра Vacancy.
+        """
+        return (f'Vacancy(name={self.name}, salary_from={self.salary_from}, salary_to={self.salary_to}, '
+                f'salary_currency={self.salary_currency}, city={self.city}, description={self.description}, '
+                f'url_vacancy={self.url_vacancy})')
