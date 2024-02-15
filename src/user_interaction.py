7fbc5bdb613cb1d0b795a18830679c791d9256b0
good_chemist <iolgasergeevna@yandex.ru>
2024-02-15 13:44:33 +0800

    display of all vacancies, function for selecting to save or not vacancies to file

--- src/user_interaction.py
+++ src/user_interaction.py
@@ -1,4 +1,29 @@
 from src.headhunterapi import HeadHunterAPI
+from src.jsonsaver import JSONSaver
+from src.vacancy import Vacancy
+
+
+def save_vacancies(vacancies: list) -> None:
+    """
+    Спрашивает у пользователя сохранить ли вакансии в файл, выполняет нужное действие.
+    :param vacancies: список с вакансиями.
+    :return: None.
+    """
+    while True:
+        user_input = input("Сохранить вакансии в файл?\n(0 - да, 1 - нет)").strip()
+
+        if user_input == '0':
+            json_saver = JSONSaver()
+            json_saver.save_to_json(vacancies)
+            break
+
+        elif user_input == '1':
+            break
+
+        else:
+            print("Попробуйте еще раз.")
+            continue
+
 
 # Функция для взаимодействия с пользователем
 def user_interaction():
@@ -11,8 +36,12 @@ def user_interaction():
     # Получение нужных вакансий с HH.ru в формате JSON
     hh_vacancies = hh_api.get_vacancies(search_query)
 
+    # Преобразует набор данных из JSON в список объектов класса Vacancy
+    vacancies_list = Vacancy.cast_to_object_list(hh_vacancies)
+
     # Цикл для выбора нужного варианта пользователем
     while True:
+
         user_choice = input("""
         0 - Показать все вакансии
         1 - Указать город для поиска
@@ -22,17 +51,25 @@ def user_interaction():
         Выберите нужный вариант: """).strip()
 
         if user_choice == '0':
-            pass
+            for item in vacancies_list:
+                print(item)
+
+            save_vacancies(vacancies_list)
+
         elif user_choice == '1':
             city_for_sorting = input("Введите название города: ")
             pass
+
         elif user_choice == '2':
             pass
+
         elif user_choice == '3':
             top_n = int(input("Введите количество вакансий для вывода в топ N: "))
             pass
+
         elif user_choice == '4':
             break  # exit()
+
         else:
             print("Попробуйте еще раз.")
             continue
