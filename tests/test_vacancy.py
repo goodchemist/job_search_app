f288411591ee867a2fc9e0e2d2699315b6cedb58
good_chemist <iolgasergeevna@yandex.ru>
2024-02-19 14:03:44 +0800

    add tests for method calculate_max_salary

--- tests/test_vacancy.py
+++ tests/test_vacancy.py
@@ -4,6 +4,9 @@ from src.vacancy import Vacancy
 
 @pytest.fixture
 def vacancy():
+    """
+    Фикстура для создания экземпляра класса Vacancy.
+    """
     return Vacancy(name='Software Engineer', salary_from=50000, salary_to=80000,
                    salary_currency='RUR', city='Moscow', description='Python developer',
                    url_vacancy='www.hh.ru')
@@ -27,3 +30,28 @@ def test_calculate_max_salary_when_both_salaries_provided(vacancy):
     Проверяет работу метода calculate_max_salary, когда указаны salary_to, salary_from.
     """
     assert Vacancy.calculate_max_salary(vacancy) == 80000
+
+
+def test_calculate_max_salary_when_only_salary_from_provided(vacancy):
+    """
+    Проверяет работу метода calculate_max_salary, когда не указана salary_to.
+    """
+    vacancy.salary_to = '--'
+    assert Vacancy.calculate_max_salary(vacancy) == 50000
+
+
+def test_calculate_max_salary_when_only_salary_to_provided(vacancy):
+    """
+    Проверяет работу метода calculate_max_salary, когда не указана salary_from.
+    """
+    vacancy.salary_from = '--'
+    assert Vacancy.calculate_max_salary(vacancy) == 80000
+
+
+def test_calculate_max_salary_when_both_salaries_not_provided(vacancy):
+    """
+    Проверяет работу метода calculate_max_salary, когда указаны не salary_to, salary_from.
+    """
+    vacancy.salary_from = '--'
+    vacancy.salary_to = '--'
+    assert Vacancy.calculate_max_salary(vacancy) == 0
